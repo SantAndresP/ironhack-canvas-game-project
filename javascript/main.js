16bb0f2f0815297c95fc5bd36841563eacf91dcb
Sant <santiagoandres@outlook.de>
2020-09-28 20:39:30 +0200

    UPDATING main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 /* Enviroment initialisation. */
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const background = (canvas.style.background = "White");
+const background = (canvas.style.background = "Black");
 canvas.style.border = "1px solid Black";
 
 /* Ball. */
@@ -64,7 +64,7 @@ class Gap {
     this.height = height;
     this.x = Math.floor(Math.random() * (canvas.width - this.width));
     this.y = y;
-    this.color = "White";
+    this.color = background;
   }
 
   draw() {
@@ -90,12 +90,13 @@ document.addEventListener("keydown", (event) => {
 });
 
 /* Game assets. */
-const ball = new Ball(12, 100, 10, "LightBlue");
+const ball = new Ball(12, 100, 10, "#FF00FF");
 const yTile = canvas.height - 18;
-const initialTile = new Tile(0, yTile, canvas.width, 20, "Black");
-const gap = new Gap(initialTile.y, 48, 20);
+const initialTile = new Tile(0, yTile, canvas.width, 10, "#39ff14");
+const gap = new Gap(initialTile.y, 40, 10);
 const tiles = [initialTile];
 tiles[0].addGap(gap);
+let j = 0;
 
 /* Main game function. */
 function startGame() {
@@ -109,16 +110,11 @@ function startGame() {
     tiles[i].gap.y--;
 
     if (tiles[i].y == 500) {
-      const tile = new Tile(0, yTile, canvas.width, 20, "Black");
-      const gap = new Gap(tile.y, 48, 20);
+      const tile = new Tile(0, yTile, canvas.width, 10, "#39FF14");
+      const gap = new Gap(tile.y, 40, 10);
       tile.addGap(gap);
       tiles.push(tile);
     }
-
-    if (tiles.length >= 30) {
-      tiles.shift();
-    }
-    console.log(tiles.length);
   }
 
   // Ball movement.
@@ -128,12 +124,31 @@ function startGame() {
     ball.moveLeft();
   }
 
-  // Losing condition.
-  if (ball.y < canvas.height - ball.radius) {
+  // Collision logic.
+  if (
+    ball.x > tiles[j].gap.x &&
+    ball.x < tiles[j].gap.x + tiles[j].gap.width &&
+    ball.y === tiles[j].gap.y
+  ) {
+    j += 1;
+    ball.y += 1;
+  } else if (ball.y === tiles[j].y) {
+    ball.y -= 1;
+  }
+
+  if (ball.y < tiles[j].y - ball.radius) {
     ball.y += 1;
   }
 
+  // Losing condition.
+  if (ball.y === 0) {
+    clearInterval(gameInterval);
+    alert("GAME OVER");
+    location.reload();
+  }
+
   ball.draw();
+  console.log(j);
 }
 
 /* Main interval function. */

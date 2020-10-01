71952789c5bece113790d719e62a5d1c6ea0b2fa
Sant <santiagoandres@outlook.de>
2020-10-01 17:21:17 +0200

    Changing audio.js

--- javascript/audio.js
+++ javascript/audio.js
@@ -1,8 +1,8 @@
 /* Audio */
 const lonelyMountain = new Audio("styles/sounds/Lonely_Mountain.ogg");
 const cottsFace = new Audio("styles/sounds/Cotts_Face.ogg");
-const pinkLady = new Audio("styles/sounds/Pink_Lady.ogg");
+const runningOne = new Audio("styles/sounds/Running_One.ogg");
 const toTheStars = new Audio("styles/sounds/To_the_Stars.ogg");
 
-const songs = [lonelyMountain, cottsFace, pinkLady, toTheStars];
+const songs = [lonelyMountain, cottsFace, runningOne, toTheStars];
 const currentSong = songs[Math.floor(Math.random() * songs.length)];

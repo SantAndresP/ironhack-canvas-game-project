903eeb4da0bb486db8c8e446cd02f42be7a2dc28
Santiago Andrés Ardiles Páez <santiagoandres@outlook.de>
2020-11-21 19:32:49 +0100

    Testing git on Windows again.

--- javascript/audio.js
+++ javascript/audio.js
@@ -1,8 +1,12 @@
-/* Audio */
+/* Audio. */
+
+// Files.
 const lonelyMountain = new Audio("styles/sounds/Lonely_Mountain.ogg");
 const cottsFace = new Audio("styles/sounds/Cotts_Face.ogg");
 const runningOne = new Audio("styles/sounds/Running_One.ogg");
 const toTheStars = new Audio("styles/sounds/To_the_Stars.ogg");
 
 const songs = [lonelyMountain, cottsFace, runningOne, toTheStars];
+
+// Selects a random song from `songs`.
 const currentSong = songs[Math.floor(Math.random() * songs.length)];
